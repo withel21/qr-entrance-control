22ec3d838198163f9da3d44f8277007492666622
withel21 <withel21@gmail.com>
2022-03-07 11:27:08 +0900

    commit control-server skeleton code

---
+++ qr-entrance-control-prototpye/server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const {v4 : uuid} = require("uuid");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5002;
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(cors());
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
