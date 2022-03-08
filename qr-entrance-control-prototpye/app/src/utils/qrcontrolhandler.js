5ffacf04d097707b1cc60d62359711124ea73e30
withel21 <withel21@gmail.com>
2022-03-08 17:12:24 +0900

    qr control handlers draft - need to more consideration??

--- qr-entrance-control-prototpye/app/src/utils/qrcontrolhandler.js
+++ qr-entrance-control-prototpye/app/src/utils/qrcontrolhandler.js
@@ -1,1 +1,17 @@
-//export const 
+let QrControlHandlers = [];
+
+export const registerQrControlHandler = (type, handler) => {
+  const handler = { type, handler };
+  QrControlHandlers.push(handler);
+};
+
+export const getQrControlHandler = (type) => {
+  const handler = QrControlHandlers.find(item => item.type === type);
+
+  return handler;
+};
+
+export const popQrControlHandler = (type) => {
+  const handler = getQrControlHandler(type);
+  QrControlHandlers = QrControlHandlers.filter(item => item.type !== type);
+};
