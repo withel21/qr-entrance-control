5dbf31a7cc6bc6a8a26c7bc3fc9b02338389013d
withel21 <withel21@gmail.com>
2022-03-08 15:34:40 +0900

    history key mapping

--- qr-entrance-control-prototpye/app/src/ControlPage/ControlHistory.js
+++ qr-entrance-control-prototpye/app/src/ControlPage/ControlHistory.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ControlHistory = ({history}) => {
-  const historyItems = history.map( (item)=> <p>{item}</p>);
+  const historyItems = history.map( (item, index) => 
+    <p key={(history.length - index).toString()}>
+      {item}
+    </p>
+  );
   return (
     <div>
       {historyItems}
